0faecb95b675ba4808c17b06cf4f808e4367002d
HuiSung <huisung@actionfit.kr>
2025-03-04 18:20:31 +0900

    Update OpenUPMInstaller.cs

--- Editor/Scripts/OpenUPMInstaller.cs
+++ Editor/Scripts/OpenUPMInstaller.cs
@@ -1,7 +1,7 @@
+
 using System;
 using System.IO;
 using System.Threading.Tasks;
-using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEngine;
 
@@ -9,17 +9,13 @@ namespace ActionFit.PackageInstaller
 {
     public class OpenUPMInstaller
     {
-        // manifest.json 파일 경로
-        private static readonly string manifestPath = Path.Combine(Application.dataPath, "../Packages/manifest.json");
+        private static readonly string ManifestPath = Path.Combine(Application.dataPath, "../Packages/manifest.json");
 
         public async Task InstallPackage(string packageName)
         {
             try
             {
-                // OpenUPM 레지스트리 추가 및 패키지 설치
-                EnsureOpenUPMRegistry(packageName);
-                
-                // 패키지 설치 상태 확인
+                AddPackage(packageName);
                 await WaitForPackageInstallation(packageName);
             }
             catch (Exception ex)
@@ -29,93 +25,10 @@ namespace ActionFit.PackageInstaller
             }
         }
 
-        private void EnsureOpenUPMRegistry(string packageName)
+        private void AddPackage(string packageName)
         {
-            if (!File.Exists(manifestPath))
-            {
-                throw new FileNotFoundException("manifest.json 파일을 찾을 수 없습니다.");
-            }
-
-            // manifest.json 읽기
-            string jsonText = File.ReadAllText(manifestPath);
-            var manifest = JsonUtility.FromJson<ManifestJson>(jsonText);
-            bool modified = false;
-
-            // 직접 JSON 텍스트를 수정하는 방식으로 변경
-            if (!jsonText.Contains("\"scopedRegistries\""))
-            {
-                // scopedRegistries가 없는 경우 새로 추가
-                string scope = GetScopeFromPackageName(packageName);
-                string registryContent = 
-                    "  \"scopedRegistries\": [\n" +
-                    "    {\n" +
-                    "      \"name\": \"OpenUPM\",\n" +
-                    "      \"url\": \"https://package.openupm.com\",\n" +
-                    "      \"scopes\": [\n" +
-                    $"        \"{scope}\"\n" +
-                    "      ]\n" +
-                    "    }\n" +
-                    "  ],\n";
-                
-                jsonText = jsonText.Replace("{", "{\n" + registryContent);
-                modified = true;
-            }
-            else if (!jsonText.Contains("\"url\": \"https://package.openupm.com\""))
-            {
-                // OpenUPM registry가 없는 경우 추가
-                string scope = GetScopeFromPackageName(packageName);
-                string registryEntry = 
-                    "    {\n" +
-                    "      \"name\": \"OpenUPM\",\n" +
-                    "      \"url\": \"https://package.openupm.com\",\n" +
-                    "      \"scopes\": [\n" +
-                    $"        \"{scope}\"\n" +
-                    "      ]\n" +
-                    "    },\n";
-                
-                int index = jsonText.IndexOf("\"scopedRegistries\": [") + "\"scopedRegistries\": [".Length;
-                jsonText = jsonText.Insert(index, "\n" + registryEntry);
-                modified = true;
-            }
-            else
-            {
-                // OpenUPM registry가 있는 경우 scope 추가
-                string scope = GetScopeFromPackageName(packageName);
-                if (!jsonText.Contains($"\"{scope}\""))
-                {
-                    int startIndex = jsonText.IndexOf("\"url\": \"https://package.openupm.com\"");
-                    int scopesIndex = jsonText.IndexOf("\"scopes\": [", startIndex);
-                    int scopesEndIndex = jsonText.IndexOf("]", scopesIndex);
-                    
-                    // 마지막 scope 다음에 새 scope 추가
-                    string scopeEntry = $",\n        \"{scope}\"";
-                    jsonText = jsonText.Insert(scopesEndIndex, scopeEntry);
-                    modified = true;
-                }
-            }
-
-            // 변경된 경우 파일 저장
-            if (modified)
-            {
-                File.WriteAllText(manifestPath, jsonText);
-                Debug.Log("manifest.json을 업데이트했습니다.");
-                AssetDatabase.Refresh();
-            }
-            
-            // 패키지 설치
             Debug.Log($"패키지 설치 중: {packageName}");
             Client.Add(packageName);
-        }
-        
-        // 패키지 이름에서 스코프 추출 (com.company.package -> com.company)
-        private string GetScopeFromPackageName(string packageName)
-        {
-            int lastDotIndex = packageName.LastIndexOf('.');
-            if (lastDotIndex > 0)
-            {
-                return packageName.Substring(0, lastDotIndex);
-            }
-            return packageName;
         }
         
         private async Task WaitForPackageInstallation(string packageName)
@@ -159,19 +72,6 @@ namespace ActionFit.PackageInstaller
             }
             
             return false;
-        }
-        
-        // JSON 직렬화를 위한 클래스 (필요한 경우만 사용)
-        [Serializable]
-        private class ManifestJson
-        {
-            public DependenciesJson dependencies;
-        }
-        
-        [Serializable]
-        private class DependenciesJson
-        {
-            // 동적 필드를 위해 비워둠
         }
     }
 }
